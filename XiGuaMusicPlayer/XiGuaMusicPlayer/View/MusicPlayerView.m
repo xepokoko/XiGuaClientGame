6930d0fd6a30d398a53ddb8787890ff15e9fd1c1
xepokoko <583069004@qq.com>
2022-04-16 21:36:10 +0800

    chore:app下方的播放控制view基本写好，并留出方法接口。

---
+++ XiGuaMusicPlayer/XiGuaMusicPlayer/View/MusicPlayerView.m
@@ -0,0 +1,20 @@
+//
+//  MusicPlayerView.m
+//  XiGuaMusicPlayer
+//
+//  Created by 谢恩平 on 2022/4/16.
+//
+
+#import "MusicPlayerView.h"
+
+@implementation MusicPlayerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
